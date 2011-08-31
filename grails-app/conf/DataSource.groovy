1b17d203e8c187c918910b5b8fb2ee3ee1359a12
unknown <Neoz@.(none)>
2011-08-31 16:50:24 +0700

    multitenant cache

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -5,10 +5,9 @@ dataSource {
     password = ""
 }
 hibernate {
-    cache.use_second_level_cache = false //true
-    cache.use_query_cache = false //true
-    //cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-    //cache.provider_class = 'grails.plugin.multitenant.ehcache.cache.MultiTenantEhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'grails.plugin.multitenant.ehcache.cache.MultiTenantEhCacheProvider'
 }
 // environment specific settings
 environments {
--- grails-app/controllers/law/SuitFolderController.groovy
+++ grails-app/controllers/law/SuitFolderController.groovy
@@ -2,11 +2,12 @@ package law
 
 class SuitFolderController {
   def session
-  static scaffold = true
-  
+
+
   def changeTenant = {
     session["ten"] = params.id
     flash.message = "tenant change to: ${params.id}"
     redirect([action: 'list'])
   }
+   static scaffold = true
 }
